6a654ebfe8008b55e9da787c31946d7978f0c7b9
john1256 <john071200@naver.com>
2025-09-15 01:05:44 +0900

    assignment 2 prob 2, 5

---
+++ 2025/Statistics_1/func.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def mean(data):
+    return np.sum(data)/len(data)
+def geom_mean(data):
+    if np.any(data<=0):
+        return np.nan
+    else:
+        return np.exp(np.sum(np.log(data))/len(data))
+def median(data):
+    sort = np.sort(data)
+    n = len(data)
+    if n%2==0:
+        return (sort[n//2-1]+sort[n//2])/2
+    else:
+        return sort[n//2]
+def variance(data):
+    return mean(data**2)-mean(data)**2
+def std(data):
+    return np.sqrt(variance(data))
+def skewness(data):
+    x3 = mean(data**3)
+    x2 = mean(data**2)
+    x = mean(data)
+    sig = std(data)
+    return 1/(sig**3)*(x3 - 3*x*x2 + 2*x**3)
+def kurtosis(data):
+    mu = mean(data)
+    return mean((data - mu)**4) / (variance(data)**2) - 3
