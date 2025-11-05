fd9e852455db9e77b6d8086c968bb4e9a9168ab9
john1256 <john071200@naver.com>
2025-11-05 16:24:30 +0900

    k

--- 2025/Field_research/test.py
+++
@@ -1,8 +0,0 @@
-import os
-import numpy as np
-CurrentPath = os.getcwd()
-print(CurrentPath)
-dir = os.path.dirname(__file__)
-print("Test completed successfully.")
-with open('./Results/hello.txt', 'w') as f:
-    f.write('Hello, world!')
